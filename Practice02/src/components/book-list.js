24b0f0caebea6ab16a972bff93de550df42caf9c
changmin Lee <lcmini6528@gmail.com>
2017-06-09 16:33:14 +0900

    BindDispatchToProps

--- Practice02/src/components/book-list.js
+++ Practice02/src/components/book-list.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
-export default class BookList extend Component{
+class BookList extends Component{
 
 	renderList(){
 		return this.props.books.map((book) => {
@@ -11,10 +14,21 @@ export default class BookList extend Component{
 	}
 
 	render(){
-		return {
+		return (
 			<ul className="list-group col-sm-4">
 				{this.renderList()}
 			</ul>
-		}
+		)
 	}
 }
+
+function mapStateToProps(state){
+	return {
+		books:state.books
+	};
+}
+
+function mapDispatchToProps(dispatch){
+	return bindActionCreators({ selectBook : selectBook },dispatch)
+}
+export default connect(mapStateToProps,mapDispatchToProps)(BookList);
