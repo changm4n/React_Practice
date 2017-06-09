933f9b92c0ccb49b405a0f399d13485970674772
changmin lee <lcmini6528@gmail.com>
2017-06-10 02:55:49 +0900

    Create Action

--- Practice02/src/components/app.js
+++ Practice02/src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from './book-list';
+import BookDetail from './book-detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
       	<BookList />
+        <BookDetail />
       </div>
     );
   }
