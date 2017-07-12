980f6c48f5c932ff581ffd3dbd9f7f0d2660944d
changmin Lee <lcmini6528@gmail.com>
2017-07-12 16:02:00 +0900

    add Link

--- Practice04/src/index.js
+++ Practice04/src/index.js
@@ -2,29 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
-import App from './components/app';
 import reducers from './reducers';
 
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
-class Hello extends React.Component{
-	render(){ return <div>Hello!</div>}
-}
-class Goodbye extends React.Component{
-	render(){ return <div>Goodbye!</div>}
-}
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     <div>
-    	Header
-    	<Route path="/hello" component={Hello} />
-    	<Route path="/goodbye" component={Goodbye} />
-	</div>
+      <Switch>
+      <Route path="/posts/new" component={PostsNew} />
+    	<Route path="/" component={PostsIndex} />
+      </Switch>
+	 </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
